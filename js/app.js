78cc31db619026b8ecb51a349d4b677656c4b5ba
giovanni2299 <giovanni-sorrentino@hotmail.it>
2024-03-08 14:22:40 +0100

    ADD:start whit the js part

---
+++ js/app.js
@@ -0,0 +1,14 @@
+// Consegna:
+// Dato un array contenente una lista di cinque immagini, creare un carosello come nello screenshot allegato.
+// MILESTONE 1
+// Per prima cosa, creiamo il markup statico: costruiamo il container e inseriamo un'immagine grande al centro: 
+// avremo così la struttura base e gli stili pronti per poterci poi concentrare solamente sull'aspetto logico.
+
+const imagesArray = [
+'./img/01.webp', 
+'./img/02.webp', 
+'./img/02.webp', 
+'./img/03.webp', 
+'./img/04.webp', 
+'./img/05.webp'
+]
